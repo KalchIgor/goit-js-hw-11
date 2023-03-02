a14980b2a943831997f2b418c135ab9ec4668b8e
Игорь Кальчин <108684879+KalchIgor@users.noreply.github.com>
2023-03-02 22:29:50 +0200

    css

---
+++ src/js/pixabay.js
@@ -0,0 +1,12 @@
+import axios from "axios";
+
+const API_KEY = "34077503-5bfdb6066ba2aa1107947cc02";
+
+
+var URL = "https://pixabay.com/api/?key="+API_KEY+"&q="+encodeURIComponent('red roses');
+$.getJSON(URL, function(data){
+if (parseInt(data.totalHits) > 0)
+    $.each(data.hits, function(i, hit){ console.log(hit.pageURL); });
+else
+    console.log('No hits');
+});

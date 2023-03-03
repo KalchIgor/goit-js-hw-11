b1747ee87aaf9652ccdae77d3ff76186a7ede262
Игорь Кальчин <108684879+KalchIgor@users.noreply.github.com>
2023-03-03 14:19:04 +0200

    js

--- src/js/pixabay.js
+++ src/js/pixabay.js
@@ -1,12 +1,13 @@
 import axios from "axios";
 
 const API_KEY = "34077503-5bfdb6066ba2aa1107947cc02";
+/*const BASE_URL = 'https://pixabay.com/api/';*/
+export const ITEMS_PER_PAGE = 40;
+axios.defaults.baseURL = 'https://pixabay.com/api/';
 
-
-var URL = "https://pixabay.com/api/?key="+API_KEY+"&q="+encodeURIComponent('red roses');
-$.getJSON(URL, function(data){
-if (parseInt(data.totalHits) > 0)
-    $.each(data.hits, function(i, hit){ console.log(hit.pageURL); });
-else
-    console.log('No hits');
-});
+export async function fetchArticles(query, page, perPage) {
+  const response = await axios.get(
+    `?key=${API_KEY}&q=${query}&image_type=photo&orientation=horizontal&safesearch=true&page=${page}&per_page=${perPage}`,
+  );
+  return response
+}
